0b9d2b51250b003396b3f9bdb834c80afb592984
zen <zenon.deviagge@gmail.com>
2024-04-12 18:54:40 -0300

    Create basic component structure. Import FaBars icon for the button.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,17 @@
+import { FaBars } from "react-icons/fa"
+import {useGlobalContext} from './Context'
 const Navbar = () => {
+  const {openSidebar} = useGlobalContext();
   return (
-    <h2>Navbar</h2>
+    <nav>
+    <div className="nav-center">
+      <h3 className="logo">strapi</h3>
+      <button className="toggle-btn" onClick={openSidebar}>
+        <FaBars/>
+      </button>
+      {/* nav links later */}
+    </div>
+    </nav>
   )
 }
 export default Navbar
