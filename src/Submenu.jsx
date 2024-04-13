af6188a01d24cb55ebef9efc2bcd9fee6ddd2f2a
zen <zenon.deviagge@gmail.com>
2024-04-13 12:20:09 -0300

    Display raw currentPage properties. Use optional chaining.

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -2,11 +2,27 @@ import { useGlobalContext } from './Context'
 import sublinks from './data'
 
 const Submenu = () => {
-  const {pageId} = useGlobalContext();
-  const currentPage = sublinks.find((item)=> item.pageId === pageId)
-  console.log(currentPage);
+  const { pageId } = useGlobalContext()
+  const currentPage = sublinks.find((item) => item.pageId === pageId)
   return (
-    <h2>Submenu</h2>
+    <div className="submenu">
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const {id, url, label, icon} = link
+          return <a key={id} href={url}>
+            {icon}
+            {label}
+          </a>
+        })}
+      </div>
+    </div>
   )
 }
 export default Submenu
