6fda1ba7d0dfc8e80174e5c1a3937a21c94e8a2b
zen <zenon.deviagge@gmail.com>
2024-04-13 00:24:36 -0300

    Create NavLinks component. Nest it on Navbar component.

---
+++ src/NavLinks.jsx
@@ -0,0 +1,17 @@
+import sublinks from './data'
+
+const NavLinks = () => {
+  return (
+    <div className="nav-links">
+      {sublinks.map((item) => {
+        const { pageId, page } = item
+        return (
+          <button key={pageId} className="nav-link">
+            {page}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
+export default NavLinks
