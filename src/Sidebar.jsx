74cec47f441c6eb91b87f2492ef1aef24ece631f
zen <zenon.deviagge@gmail.com>
2024-04-12 23:45:30 -0300

    Import sublinks from data to Sidebar component. Display raw data content.

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,34 @@
+import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './Context'
+import sublinks from './data'
+
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
   return (
-    <h2>Sidebar</h2>
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item) => {
+            const {links, page, pageId} = item;
+            return <article key={pageId}>
+              <h4>{page}</h4>
+              <div className="sidebar-sublinks">
+                {links.map((link) => {
+                  const {url, icon, label, id} = link
+                  return <a key={id} href={url}>
+                    {icon}
+                    {label}
+                  </a>
+                })}
+              </div>
+            </article>
+          })}
+        </div>
+      </div>
+    </aside>
   )
 }
 export default Sidebar
