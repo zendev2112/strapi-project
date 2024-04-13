8c41a9be8466692f61e46084aa01b6d53da029ad
zen <zenon.deviagge@gmail.com>
2024-04-13 15:25:32 -0300

    Style Submenu. Add show-submenu class for toggle functionality.

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -5,7 +5,7 @@ const Submenu = () => {
   const { pageId } = useGlobalContext()
   const currentPage = sublinks.find((item) => item.pageId === pageId)
   return (
-    <div className="submenu">
+    <div className={currentPage ? 'submenu show-submenu' : 'submenu'}>
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
