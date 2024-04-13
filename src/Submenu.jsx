c079f65c5db311c29f926c08a5eb0d1fc34b1a3c
zen <zenon.deviagge@gmail.com>
2024-04-13 11:42:45 -0300

    Import pageId state variable from useGlobalContext custom hook. Set currentPage variable using find() to match pageId state variable with current item.pageId. Log currentPAge object on console.

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,4 +1,10 @@
+import { useGlobalContext } from './Context'
+import sublinks from './data'
+
 const Submenu = () => {
+  const {pageId} = useGlobalContext();
+  const currentPage = sublinks.find((item)=> item.pageId === pageId)
+  console.log(currentPage);
   return (
     <h2>Submenu</h2>
   )
