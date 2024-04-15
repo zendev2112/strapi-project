8452ee9bd3f4b94a45a69eb6bbd14ef5ec3d6a53
zen <zenon.deviagge@gmail.com>
2024-04-15 18:53:39 -0300

    Setup hide Submenu component funcionality

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,11 +1,28 @@
 import { useGlobalContext } from './Context'
 import sublinks from './data'
+import { useRef } from 'react'
 
 const Submenu = () => {
-  const { pageId } = useGlobalContext()
+  const { pageId, setPageId } = useGlobalContext()
   const currentPage = sublinks.find((item) => item.pageId === pageId)
+
+  const submenuContainer = useRef(null)
+
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current;
+    const {left, right, bottom} = submenu.getBoundingClientRect()
+    const { clientX, clientY } = event
+
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null)
+    }
+  }
   return (
-    <div className={currentPage ? 'submenu show-submenu' : 'submenu'}>
+    <div
+      className={currentPage ? 'submenu show-submenu' : 'submenu'}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
@@ -15,11 +32,13 @@ const Submenu = () => {
         }}
       >
         {currentPage?.links?.map((link) => {
-          const {id, url, label, icon} = link
-          return <a key={id} href={url}>
-            {icon}
-            {label}
-          </a>
+          const { id, url, label, icon } = link
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          )
         })}
       </div>
     </div>
