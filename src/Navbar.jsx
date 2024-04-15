6d49bc40f941dc57e26b8f28518ac28ae3bb7c27
zen <zenon.deviagge@gmail.com>
2024-04-15 18:54:58 -0300

    Finish project and upload. Fix ommited z-index in .show-submenu selector on CSS

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,22 @@
-import { FaBars } from "react-icons/fa"
-import {useGlobalContext} from './Context'
+import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './Context'
+import NavLinks from './NavLinks'
 const Navbar = () => {
-  const {openSidebar} = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext()
+  const handleSubmenu = (e) => {
+    if(!e.target.classList.contains('nav-link')) {
+      setPageId(null)
+    }
+  }
   return (
-    <nav>
-    <div className="nav-center">
-      <h3 className="logo">strapi</h3>
-      <button className="toggle-btn" onClick={openSidebar}>
-        <FaBars/>
-      </button>
-      {/* nav links later */}
-    </div>
+    <nav onMouseOver={handleSubmenu}>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        <NavLinks />
+      </div>
     </nav>
   )
 }
